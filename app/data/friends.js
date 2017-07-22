39756d9d2c4e59ac0b1e836a4314e04030324dde
Frank Mcdonald <frankmcd@cox.net>
2017-07-22 08:25:40 -0700

    file commits

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [{
+    name: 'Darth Vader',
+    profilePic: 'http://www.space.ca/wp-content/uploads/2016/06/darth-vader-crossed-arms-1280jpg-88461e1280wjpg-67c0c2_1280w.jpg',
+    scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+    ]
+
+}]
+module.exports = friends;
