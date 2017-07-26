69bc800a30a90089cddb1d5de622d7cdf983c1c6
Frank Mcdonald <frankmcd@cox.net>
2017-07-26 16:18:41 -0700

    added to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,35 @@
 var friends = [{
-    name: 'Darth Vader',
-    profilePic: 'http://www.space.ca/wp-content/uploads/2016/06/darth-vader-crossed-arms-1280jpg-88461e1280wjpg-67c0c2_1280w.jpg',
-    scores: [
-        "4",
-        "5",
-        "5",
-        "2",
-        "4",
-        "5",
-        "4",
-        "4",
-        "3",
-        "4"
-    ]
+        "name": "Darth Vader",
+        "profilePic": "http://www.space.ca/wp-content/uploads/2016/06/darth-vader-crossed-arms-1280jpg-88461e1280wjpg-67c0c2_1280w.jpg",
+        "scores": [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "Luke SkyWalker",
+        "image": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/03/04/12/luke-force-awakens.jpg",
+        "responses": [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "5",
+            "5",
+            "5",
+            "5"
+        ],
 
-}]
+    }
+]
 module.exports = friends;
